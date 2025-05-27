18b0a325e0f80f9341f8750fe579c63e93ce47d5
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2025-05-27 16:52:46 +0300

    test: Add enough new object when path goes nowhere

--- src/lib.rs
+++ src/lib.rs
@@ -66,4 +66,25 @@ mod tests {
         data.update_or_create("", Value::Bool(true)).unwrap();
         assert!(data.as_bool().unwrap());
     }
+
+    #[test]
+    fn insert_new_objects() {
+        let mut data = json!({});
+        data.update_or_create("a.b.c.d.e.f", 543.into()).unwrap();
+        assert_eq!(543, data["a"]["b"]["c"]["d"]["e"]["f"]);
+    }
+
+    #[test]
+    fn add_enough_elements() {
+        let mut data = json!({});
+        data.update_or_create("array1", json!([])).unwrap();
+        assert!(data["array1"].is_array());
+        let n = 501;
+        data.update_or_create(format!("array1.{}", n), true.into())
+            .unwrap();
+        for i in 0..(n - 1) {
+            assert!(data["array1"][i].is_null());
+        }
+        assert!(data["array1"][n].as_bool().unwrap());
+    }
 }
